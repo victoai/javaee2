ffaccc31b1e860e6cd8c7173b2f5a77c31ee31fb
jooyeon <victoai999@gmail.com>
2023-08-27 23:39:44 +0900

    웹3일차

---
+++ mvc/src/main/java/mvc/TEst.java
@@ -0,0 +1,15 @@
+package mvc;
+
+public class TEst {
+
+	public static void main(String[] args) {
+		int sum=0, i=0;
+		while( i<20){
+		  sum= sum+i;
+		  i +=2;
+		}
+		System.out.println( sum );
+
+	}
+
+}
